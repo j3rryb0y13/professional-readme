31101e1eda597b6d51d4571aa3c244f8cba2cb84
Jeremy Koehler <20194151+j3rryb0y13@users.noreply.github.com>
2024-05-16 19:40:13 -0400

    half of criteria met with descritpiion information

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./develop/utils/generateMarkdown');
-
+const { type } = require('os');
+const { default: Choice } = require('inquirer/lib/objects/choice');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -10,7 +12,49 @@ const questions = [
     type: 'input',
     name: 'title',
     message: 'What is the title of your project'
-  }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description of your project'
+  },
+  {
+    type: 'input',
+    name: 'installatioin',
+    message: 'Please provide installation instructions'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide usuage information'
+  },
+  {
+    type: 'input',
+    name: 'contributing'
+    message: 'Please provide contributiion guideline'
+  },
+  {
+    type: 'input',
+    name: 'tests'
+    message: 'Please provide test instructions'
+  },
+  {
+    type: 'input',
+    name: 'license'
+    message: 'Please choose a license for your project'
+    Choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+  },
+  {
+    type: 'input',
+    name: 'github'
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  },
+  // Add more questions here for other project information
 ];
 
 // TODO: Create a function to write README file
@@ -28,11 +72,12 @@ function writeToFile(fileName, data) {
 function init() {
   inquirer.prompt(questions)
     .then((answers) => {
+      console.log('Data Object:', answers);
       const readmeContent = generateMarkdown(answers);
-      writeToFile('README.md' , readmeContent);
+      writeToFile('README.md', readmeContent);
     })
     .catch((error) => {
-      console.error('Error occured:', error);
+      console.error('Error occurred:', error);
     });
 }
 
